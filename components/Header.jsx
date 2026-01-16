94a39d0a6e04db1964c543a63ea38a2a7cae75ca
Dattasai vakiti <sunny@Dattasais-MacBook-Air.local>
2026-01-15 21:49:19 -0500

    Fix logo path and asset imports for GitHub Pages

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from "./Header.module.css";
+import logo from "../public/images/logo.svg";
 function Header() {
   return (
     <ul className={styles.header}>
       <li className={styles.list}>
-        <img className={styles.img} src="/images/logo.svg" alt="welcome icon" />
+        <img className={styles.img} src={logo} alt="welcome icon" />
       </li>
       <li className={styles.list}> units</li>
     </ul>
